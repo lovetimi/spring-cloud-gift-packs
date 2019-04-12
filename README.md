1904b2120684d463d21988ebe03684f35c188f04
van.w <xingfan_wang@live.cn>
2019-04-12 16:30:06 +0800

    init 服务发现框架eureka

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 spring-cloud狂欢大礼包
+eureka-server   //eureka 服务发现框架
---
+++ eureke-server/src/main/java/xyz/wangfan/eurekeserver/EurekeServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.wangfan.eurekeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekeServerApplication.class, args);
+    }
+
+}
