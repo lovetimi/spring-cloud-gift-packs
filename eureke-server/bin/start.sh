6d11d47f4121f90a2fe22885b5b2bf940d32a337
lovetimi <xingfan_wang@live.cn>
2020-02-27 14:08:05 +0800

    Update start.sh

--- eureke-server/bin/start.sh
+++ eureke-server/bin/start.sh
@@ -6,6 +6,6 @@ cd /server/eureke-server
 
 echo "---------------正在启动后台程序---------------"
 
-nohup java -jar eureke-server.jar &
+nohup java -jar eureke-server.jar  >> /logs/eureke-server-log.log 2>&1 &
 
 echo "---------------正在启动后台程序完成---------------"
